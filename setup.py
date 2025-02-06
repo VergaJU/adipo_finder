e29384c3eec10ced66ac9a67f6b52018a618155d
VergaJU <jacopoumberto.verga@gmail.com>
2025-02-06 10:32:45 +0100

    make it installable

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="my_module",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=required,
+    python_requires=">=3.8",
+)
