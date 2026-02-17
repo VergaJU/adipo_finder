4299dc37c51e0e44dd35d8f9cb88b3f6b7fcaaa0
VergaJU <jacopoumberto.verga@gmail.com>
2026-02-17 16:41:13 +0000

    fix API installed package

---
+++ adipo_finder/__init__.py
@@ -0,0 +1,14 @@
+from .segmentation import Segmentation
+from .features import FeatureExtraction
+from .model import AdipoModel
+from .utils import Preprocessing
+from .evaluation import Evaluation
+from .plotting import Plotting
+__all__ = [
+    "Segmentation",
+    "FeatureExtraction",
+    "AdipoModel",
+    "Preprocessing",
+    "Evaluation",
+    "Plotting"
+]
